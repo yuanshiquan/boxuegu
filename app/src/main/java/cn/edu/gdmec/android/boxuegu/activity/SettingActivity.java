f5b9857a491ed77b3a514b89675c0aeb5ff985a7
yuanshiquan <1559536367@qq.com>
2018-01-01 18:05:31 +0800

    解决一些bug

--- app/src/main/java/cn/edu/gdmec/android/boxuegu/activity/SettingActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/boxuegu/activity/SettingActivity.java
@@ -2,6 +2,7 @@ package cn.edu.gdmec.android.boxuegu.activity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -15,29 +16,39 @@ import cn.edu.gdmec.android.boxuegu.R;
 
 public class SettingActivity extends AppCompatActivity {
     public static  SettingActivity instance;
+    private TextView tv_main_title;
+    private TextView tv_back;
+    private RelativeLayout rl_title_bar;
+    private RelativeLayout rl_modiy_psw;
+    private RelativeLayout rl_security_setting;
+    private RelativeLayout rl_exit_login;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+        //设置界面为竖屏
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         instance = this;
         init();
     }
 
     private void init() {
-        TextView  tv_main_title=(TextView) findViewById(R.id.tv_main_title);
+        tv_main_title = (TextView) findViewById(R.id.tv_main_title);
         tv_main_title.setText("设置");
-        TextView  tv_back=(TextView) findViewById(R.id.tv_back);
-        RelativeLayout rl_title_bar = (RelativeLayout) findViewById(R.id.title_bar);
+        tv_back = (TextView) findViewById(R.id.tv_back);
+        rl_title_bar = (RelativeLayout) findViewById(R.id.title_bar);
         rl_title_bar.setBackgroundColor(Color.parseColor("#30B4FF"));
-        RelativeLayout rl_modiy_psw = (RelativeLayout) findViewById(R.id.rl_modiy_psw);
-        RelativeLayout rl_security_setting  = (RelativeLayout) findViewById(R.id.rl_security_setting);
-        RelativeLayout rl_exit_login  =(RelativeLayout) findViewById(R.id.rl_exit_login);
+        rl_modiy_psw = (RelativeLayout) findViewById(R.id.rl_modiy_psw);
+        rl_security_setting = (RelativeLayout) findViewById(R.id.rl_security_setting);
+        rl_exit_login = (RelativeLayout) findViewById(R.id.rl_exit_login);
         tv_back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 SettingActivity.this.finish();
             }
         });
+        //修改密码点击事件
         rl_modiy_psw.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -47,6 +58,7 @@ public class SettingActivity extends AppCompatActivity {
 
             }
         });
+        //设置密保点击事件
         rl_security_setting.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -56,6 +68,7 @@ public class SettingActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        //退出登录点击事件
         rl_exit_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -71,10 +84,10 @@ public class SettingActivity extends AppCompatActivity {
     }
 
     private void clearLoginStatus() {
-        SharedPreferences sp = getSharedPreferences("LoginInfo",MODE_PRIVATE);
+        SharedPreferences sp = getSharedPreferences("loginInfo",MODE_PRIVATE);
         SharedPreferences.Editor editor = sp.edit();
         editor.putBoolean("isLogin",false);
-        editor.putString("LoginUserName","");
+        editor.putString("loginUserName","");
         editor.commit();
 
     }
